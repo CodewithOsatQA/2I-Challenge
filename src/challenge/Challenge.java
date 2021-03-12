9fdab8b5cd2321e184c8a95c57ce8c8ac814c555
Os <otowesho@qa.com>
2021-03-12 11:34:49 +0000

    Finished challenge

---
+++ src/challenge/Challenge.java
@@ -0,0 +1,12 @@
+package challenge;
+
+public class Challenge {
+
+    public static void multipleLoop7to100(){
+        System.out.println("multiples of 7 upto One Hundred");
+        for (int i = 1; i <= 100; i++){
+            System.out.println(7*i);
+
+        }
+    }
+}
